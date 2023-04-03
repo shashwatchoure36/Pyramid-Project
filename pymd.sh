63a0b56ba447d20e87e80887a35a10defa7e3f5f
shashwatchoure365 <shashwatchoure36@gmail.com>
2023-04-03 09:49:50 +0530

    [Shashwat] Add : This script is used to create Right Pyramid

--- pymd.sh
+++ pymd.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 #Bash Shell Script to print half pyramid using *
-echo "<<<===This is Half Left Pyramid Project===>>>"
+echo "<<<===This is Half Right Pyramid Project===>>>"        
 echo "__________________________________________"
-echo -n
-read -p "Enter the Rows : " rows
+echo -n                                                                  #for next line
+read -p "Enter the Rows : " rows                                         #for user input
 
-for((i=1; i<=rows; i++))
+for((i=0; i<=rows; i++))
 do
-  for((j=1; j<=i; j++))
-  do
-    echo -n "* "
-  done
+  for((j=2*(rows-i);j>=0;j--))
+    do
+        echo -n " "
+    done
+  for((j=0;j<=i;j++))
+    do
+        echo -n "* "
+    done
   echo
 done
